a40e442a565e01892bbcbb57c3ea83231785449e
Dustin Frisch <fooker@lab.sh>
2016-03-29 00:39:59 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ubermove',
+    version='0.1',
+
+    url='https://github.com/fooker/ubermove',
+    license='GPLv3',
+    author='Dustin Frisch',
+    author_email='fooker@lab.sh',
+    description='ubermove (short umv) is a mass file management tool utilizing your favorite text editor',
+
+    install_requires=['rarfile>=2.7'],
+
+    packages=[],
+    entry_points={
+        'console_scripts': ['umv=ubermove:main'],
+    },
+)
